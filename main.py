1a95a3871492e7c9c5a70ade54033405a6b4150d
Daniel Waterworth <da.waterworth@gmail.com>
2016-06-26 15:38:01 +0100

    Better signal handling

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import bytecode.constructor
 import bytecode.printer
 import sys_calls
 import execution.executor as executor
-from rpython.rlib import rsocket
+from rpython.rlib import rsocket, rsignal
 import data
 
 data.register_all()
@@ -18,24 +18,25 @@ def print_usage(prog):
     print "  %s replay BYTECODE TRACE" % prog
 
 def entry_point(argv):
+    rsignal.pypysig_setflag(rsignal.SIGINT)
     rsocket.rsocket_startup()
 
     constructor = bytecode.constructor.BytecodeConstructor()
 
-    fd = None
     if len(argv) < 3:
         print_usage(argv[0])
         return 1
 
+    trace_fd = None
     mode = argv[1]
     if mode == 'exec':
         sys_caller = sys_calls.Perform()
     elif mode == 'trace' and len(argv) == 4:
-        fd = open(argv[3], 'w')
-        sys_caller = sys_calls.TraceProxy(sys_calls.Perform(), fd)
+        trace_fd = open(argv[3], 'w')
+        sys_caller = sys_calls.TraceProxy(sys_calls.Perform(), trace_fd)
     elif mode == 'replay' and len(argv) == 4:
-        fd = open(argv[3], 'r')
-        sys_caller = sys_calls.Replay(fd)
+        trace_fd = open(argv[3], 'r')
+        sys_caller = sys_calls.Replay(trace_fd)
     else:
         print_usage(argv[0])
         return 1
@@ -43,11 +44,12 @@ def entry_point(argv):
     with open(argv[2], 'r') as fd:
         bytecode.read.read_bytecode(fd, constructor)
 
-    ex = executor.Executor(sys_caller, constructor.get_program())
-    ex.run()
-
-    if fd:
-        fd.close()
+    try:
+        ex = executor.Executor(sys_caller, constructor.get_program())
+        ex.run()
+    finally:
+        if trace_fd:
+            trace_fd.close()
 
     return 0
 
